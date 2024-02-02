2e029a1065a29fb8ee0c7bd90a88df8080bfe55c
matias <mchialva@mail.austral.edu.ar>
2024-02-02 11:39:56 -0300

    reorganized the code in functions and cleaned the main file for future development. Before trying to automize email sent, if it breaks later, return to this commit.

--- functions.py
+++ functions.py
@@ -3,11 +3,66 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
+def add_data_dataframe(stocks, df):
+    '''given a list of stocks and a dataframe, downloads the "Close" price
+     of each stock everyday and concatenates it to the dataframe.'''
+    for stock in stocks:
+        stockDataframe = yf.download(stock, period="max")["Close"]  # columna de cotización de la stock
+        stockDataframe.name = stock  # column name is the stock name
+        df = pd.concat([df, stockDataframe], axis=1)  # mergeo data de cada stock al df
+    return df
+
 def cleaning_dataframe(df):
-    df.sort_index(ascending=True, inplace=True)
-    df.index.name = "Date"
-    df.ffill(inplace=True)
-    df.dropna(how="all", inplace=True)
+    '''clean the dataframe'''
+    df.sort_index(ascending=True, inplace=True) #lo dejo sorteado con la última fecha abajo.
+    df.index.name = "Date" #index column llamada "Date"
+    df.ffill(inplace=True) #relleno los vacios con el valor del de arriba
+    df.dropna(how="all", inplace=True) #si hay filas completamente vacias, funalas
+
+def add_price_changes(df, percentageDF):
+    '''given a dataframe with dates as index and columns for stocks, uses that information
+    to compare each stock value to its previous values and adds the price change to percentageDF.'''
+
+    for stock in df.columns:  # para cada stock
+        currentDate = df.index[-1]
+        current = df[stock].loc[currentDate]  # last price in the stock column
+
+        period_dict_days = {"1D": 1, "5D": 5}
+        for periodKey, periodValue in period_dict_days.items():  # para cada período
+            pastDate = find_closest_date(currentDate - pd.DateOffset(days=periodValue), df)
+            past = df[stock].loc[pastDate]  # calculo valor en ese momento
+            priceChange = (current - past) / past * 100  # calculo el porcentaje de cambio de precio
+            percentageDF.loc[stock, periodKey] = round(priceChange, 2)  # agrego al dataframe
+
+        period_dict_months = {"1M": 1, "3M": 3, "6M": 6}
+        for periodKey, periodValue in period_dict_months.items():  # para cada período
+            pastDate = find_closest_date(currentDate - pd.DateOffset(months=periodValue), df)
+            past = df[stock].loc[pastDate]  # calculo valor en ese momento
+            priceChange = (current - past) / past * 100  # calculo el porcentaje de cambio de precio
+            percentageDF.loc[stock, periodKey] = round(priceChange, 2)  # agrego al dataframe
+
+        period_dict_years = {"1Y": 1, "2Y": 2, "5Y": 5, "10Y": 10}
+        for periodKey, periodValue in period_dict_years.items():  # para cada período
+            pastDate = find_closest_date(currentDate - pd.DateOffset(years=periodValue), df)
+            past = df[stock].loc[pastDate]  # calculo valor en ese momento
+            priceChange = (current - past) / past * 100  # calculo el porcentaje de cambio de precio
+            percentageDF.loc[stock, periodKey] = round(priceChange, 2)  # agrego al dataframe
+
+    return percentageDF
+
+def add_sectors(sectorStockDict, percentageDF):
+    for sector, stock in sectorStockDict.items():
+        percentageDF.loc[stock, "Sector"] = sector
+    return percentageDF
+
+def style_dataframe(percentageDF):
+    '''applyies gradient to the columns i picked'''
+    columnsExcluded = ["Stock", "Sector"]
+    columnsIncluded = percentageDF.columns.difference(columnsExcluded)
+    stylePercentageDF = percentageDF.style.background_gradient(subset=columnsIncluded, cmap='RdYlGn',
+                                                               axis=0)  # applied by column not row
+    return stylePercentageDF
+
 
 def find_closest_date(current_date, df):
     closest_date = None
--- main.py
+++ main.py
@@ -19,21 +19,15 @@ sectorStockDict = {
 }
 
 #-----------------dataframe con precios diarios----------------------
-'''
+
 #DF vacío, índice = date, donde va desde 1800 hasta hoy.
 hoy = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
 df = pd.DataFrame(index=pd.date_range(start=datetime(1800, 1, 1), end=hoy, freq="B")) #freq B -> business days
 
-#agrego la data de cada stock a df
-for stock in stocks:
-    stockDataframe = yf.download(stock, period="max")["Close"] #columna de cotización de la stock
-    stockDataframe.name = stock #column name is the stock name
-    df = pd.concat([df, stockDataframe], axis=1) #mergeo data de cada stock al df
-
-#limpieza de datos
+df = add_data_dataframe(stocks, df) #lleno el df con data de cada stock
 cleaning_dataframe(df) #función que limpia datos
 df.to_excel("stocks_test.xlsx", index=True) #lo paso a un excel
-'''
+
 #-----------------dataframe con porcentajes----------------------
 df = pd.read_excel("stocks_test.xlsx", index_col="Date")
 
@@ -41,40 +35,13 @@ df = pd.read_excel("stocks_test.xlsx", index_col="Date")
 percentageDF = pd.DataFrame(index = df.columns, columns=["1D", "5D", "1M", "3M", "6M", "1Y", "2Y", "5Y", "10Y", "Sector"]) #quiero las stocks a la izquierda
 percentageDF.index.name = "Stock"
 
-#agrego columna con sector para cada stock
-for sector, stock in sectorStockDict.items():
-    percentageDF.loc[stock, "Sector"] = sector
+percentageDF = add_sectors(sectorStockDict, percentageDF) #add stock's sectors
 
-for stock in df.columns: #para cada stock
-    currentDate = df.index[-1]
-    current = df[stock].loc[currentDate] #last price in the stock column
-
-    period_dict_days = {"1D" : 1, "5D" : 5}
-    for periodKey, periodValue in period_dict_days.items(): #para cada período
-        pastDate = find_closest_date(currentDate - pd.DateOffset(days=periodValue), df)
-        past = df[stock].loc[pastDate] #calculo valor en ese momento
-        priceChange = (current - past) / past * 100 #calculo el porcentaje de cambio de precio
-        percentageDF.loc[stock, periodKey] = round(priceChange, 2) #agrego al dataframe
-
-    period_dict_months = {"1M": 1, "3M": 3, "6M": 6}
-    for periodKey, periodValue in period_dict_months.items():  # para cada período
-        pastDate = find_closest_date(currentDate - pd.DateOffset(months=periodValue), df)
-        past = df[stock].loc[pastDate]  # calculo valor en ese momento
-        priceChange = (current - past) / past * 100  # calculo el porcentaje de cambio de precio
-        percentageDF.loc[stock, periodKey] = round(priceChange, 2)  # agrego al dataframe
-
-    period_dict_years = {"1Y": 1, "2Y": 2, "5Y": 5, "10Y": 10}
-    for periodKey, periodValue in period_dict_years.items():  # para cada período
-        pastDate = find_closest_date(currentDate - pd.DateOffset(years=periodValue), df)
-        past = df[stock].loc[pastDate]  # calculo valor en ese momento
-        priceChange = (current - past) / past * 100  # calculo el porcentaje de cambio de precio
-        percentageDF.loc[stock, periodKey] = round(priceChange, 2)  # agrego al dataframe
+percentageDF = add_price_changes(df, percentageDF) #add price changes
 
 #aplico el gradiente colorido a las columnas de stocks
-columnsExcluded = ["Stock", "Sector"]
-columnsIncluded = percentageDF.columns.difference(columnsExcluded)
-stylePercentageDF = percentageDF.style.background_gradient(subset=columnsIncluded, cmap='RdYlGn', axis=0) #applied by column not row
+stylePercentageDF = style_dataframe(percentageDF)
 
 #-------------------dataframes to excel-----------------------------
 stylePercentageDF.to_excel("styledStocksPercentage.xlsx", index=True) #lo paso a un excel
-percentageDF.to_excel("stocksPercentage.xlsx", index=True) #lo paso a un excel'''
+percentageDF.to_excel("stocksPercentage.xlsx", index=True) #lo paso a un excel
